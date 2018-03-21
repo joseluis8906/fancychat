5e73f86ee66f917c06b3e136711f6168f811ef2e
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2018-03-21 14:47:06 -0500

    actualizado

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='fancychat',
-    version='1.0.6',
-    package_dir={'env': 'venv/lib/python3.5/', 'app': 'fancychat/'},
+    version='1.0.9',
+    package_dir={'env': 'venv/lib/python3.5/'},
     packages=[
-        'app.src',
+        'fancychat',
         'env.distutils',
         'env.encodings',
         'env.importlib',
@@ -64,7 +64,7 @@ setup(
     author='joseluis',
     author_email='joseluiscacere8906@gmail.com',
     description='fancy chat example',
-    scripts=['FancyChat.py'],
+    scripts=['bin/FancyChat.py'],
     install_requires=[
         'pygobject>=3.26',
         'paho-mqtt>=1.3'
